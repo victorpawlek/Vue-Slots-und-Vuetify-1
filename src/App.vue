d12fbfd28a928fc6ef11e3b5f64c8a78cf1430ca
victorpawlek <pawlek.v03@htlwienwest.at>
2021-10-17 14:13:33 +0200

    Aufgabe 3 fertig

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,149 @@
 <template>
-  <div style="display:flex">
-    <Sheet style="margin-left:20px">
-      <h3>Memo to myself</h3>
-      <p>Always prepare for PLFs in time!</p>
-      <img src="info.svg" width="80px" />
-    </Sheet>
-    <Sheet style="margin-left:20px">
-      <p>INSY, SEW and Webtechnologies are our favorite subjects!</p>
-      <span style="font-style: italic">Starkschwitz Fiedler, Jan Schreck</span>
-      <p>Wish i was there!</p>
-      <span style="font-style: italic">E. Wahn</span>
-    </Sheet>
+  <div id="app">
+    <DataTable :items="desserts" :headers="headers">
+      <template v-slot:header>
+        <h2>Watch What You Eat!</h2>
+      </template>
+      <template v-slot:footer> Created 2021 by Victor Pawlek </template>
+      <template v-slot:body.append>
+        <tr>
+          <td>Total</td>
+          <td>{{ totalCalories }}</td>
+          <td>{{ totalFat }}</td>
+          <td>{{ totalCarbs }}</td>
+          <td>{{ totalProtein }}</td>
+          <td></td>
+        </tr>
+      </template>
+    </DataTable>
   </div>
 </template>
 
 <script>
-import Sheet from '@/components/Sheet.vue';
+import DataTable from '@/components/DataTable.vue';
 export default {
   components: {
-    Sheet,
+    DataTable,
+  },
+  computed: {
+    totalCalories() {
+      let n = 0;
+      this.desserts.forEach((el) => (n += el.calories));
+      console.log(n);
+      return n;
+    },
+    totalFat() {
+      let n = 0;
+      this.desserts.forEach((el) => (n += el.fat));
+      return n;
+    },
+    totalCarbs() {
+      let n = 0;
+      this.desserts.forEach((el) => (n += el.carbs));
+      return n;
+    },
+    totalProtein() {
+      let n = 0;
+      this.desserts.forEach((el) => (n += el.protein));
+      return n;
+    },
+  },
+  data() {
+    return {
+      headers: [
+        {
+          text: 'Dessert (100g serving)',
+          value: 'name',
+        },
+        { text: 'Calories', value: 'calories' },
+        { text: 'Fat (g)', value: 'fat' },
+        { text: 'Carbs (g)', value: 'carbs' },
+        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Iron (%)', value: 'iron' },
+      ],
+      desserts: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%',
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%',
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%',
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%',
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: '16%',
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: '0%',
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: '2%',
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: '45%',
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: '22%',
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: '6%',
+        },
+      ],
+    };
   },
 };
 </script>
---
+++ src/components/DataTable.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="data-table">
+    <slot name="header"></slot>
+    <table>
+      <thead>
+        <tr>
+          <th v-for="(item, i) in headers" :key="i">{{ item.text }}</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, i) in items" :key="i">
+            <td v-for="(el, id) in item" :key="id">{{ el }}</td>
+        </tr>
+        <slot name="body.append"></slot>
+      </tbody>
+    </table>
+    <footer><slot name="footer"></slot></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['items', 'headers'],
+};
+</script>
+
+<style scoped>
+.data-table {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  margin: 40px;
+  border-collapse: collapse;
+  font-size: 0.9em;
+  font-family: sans-serif;
+  min-width: 400px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+}
+.data-table td,
+th {
+  text-align: left;
+}
+.data-table footer {
+  margin: 30px 0 30px 0;
+  font-style: italic;
+}
+.data-table thead tr {
+  background-color: #009879;
+  color: #ffffff;
+  text-align: left;
+}
+.data-table th,
+.data-table td {
+  padding: 12px 15px;
+}
+.data-table tbody tr {
+  border-bottom: 1px solid #dddddd;
+}
+.data-table tbody tr:nth-of-type(even) {
+  background-color: #f3f3f3;
+}
+</style>
